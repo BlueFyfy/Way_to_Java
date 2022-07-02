dcfea269ccec59df6f1980dff9736ed52bc6b34f
BlueFyfy <71499179+BlueFyfy@users.noreply.github.com>
2022-07-02 20:28:14 +0800

    Add files via upload

---
+++ YourInitials.java
@@ -0,0 +1,11 @@
+public class YourInitials {
+    public static void main(String[] args) {
+        System.out.println("M   M  X   X");
+        System.out.println("MM MM  X   X");
+        System.out.println("M M M   X X ");
+        System.out.println("M   M    X  ");
+        System.out.println("M   M   X X ");
+        System.out.println("M   M  X   X");
+        System.out.println("M   M  X   X");
+    }
+}
