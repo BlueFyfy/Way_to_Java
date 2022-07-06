35450211268442e0f53a20dddf3e02b589da2112
BlueFyfy <71499179+BlueFyfy@users.noreply.github.com>
2022-07-06 23:07:01 +0800

    Add files via upload

---
+++ WholeNumber.java
@@ -0,0 +1,9 @@
+public class WholeNumber{
+    public static void main(String[] args) {
+        double heightOfTree = 7.5;
+        System.out.println("The tree is " + heightOfTree + " metres tall.");
+        long population = 7000000000L;
+        System.out.println("The world's population is " + population);
+
+    }
+}
